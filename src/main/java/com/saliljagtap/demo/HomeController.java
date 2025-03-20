c747a52e1250cf1f9019e047d90dba5d8e082f78
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-20 20:32:31 +0530

    portdockadded9

--- src/main/java/com/saliljagtap/demo/HomeController.java
+++ src/main/java/com/saliljagtap/demo/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/home")
+	@GetMapping("home")
 	public String home() {
 //		System.out.println("Hii");
 		return "home";
